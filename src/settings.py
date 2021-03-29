652b6b5f1e48b8f5fe32d2a9a8e834328ac761b4
YH Song <1308293139@qq.com>
2021-03-29 21:00:28 +0800

    add config

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ class Settings:
         # settings for the bullets
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # settings for the aliens
